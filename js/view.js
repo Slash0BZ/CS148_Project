74df2495f43765c67e93b853cae40326bcac399c
s-ren <silei_ren@brown.edu>
2016-08-01 18:09:20 -0700

    add simple circular motion

--- js/view.js
+++ js/view.js
@@ -1,24 +1,8 @@
-function normalize(a) {
-  var scale = Math.sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
-  a.x = a.x / scale;
-  a.y = a.y / scale;
-  a.z = a.z / scale;
-  return a;
-}
-
-function dot(a, b) {
-  return a.x * b.x + a.y * b.y + a.z * b.z;
-}
-
-
-
 
 var gl = GL.create();
-var speed = 0;
-var movement = 0;
-var angleX = -35;
-var angleY = 45;
-var camera = new GL.Vector(5, 5, 5);
+var angleX = -23.5;
+var angleY = -2;
+var camera = new GL.Vector(1.166,6.23,20);
 var mesh = GL.Mesh.sphere({ normals: true, radius: 1, detail: 12 }).computeWireframe();
 var plane_mesh = GL.Mesh.plane({normals: true}).transform(GL.Matrix.scale(10, 10, 5));  
 var shader = new GL.Shader('\
@@ -43,8 +27,7 @@ gl.onmousemove = function(e) {
 };
 
 gl.onupdate = function(seconds) {
-  //movement = speed * seconds + 4.9  * seconds * seconds + movement;
-  //speed = seconds * 9.8 + speed;
+  moveAndUpdate(seconds);
   
   var CameraSpeed = seconds * 4;
 
@@ -64,15 +47,18 @@ gl.onupdate = function(seconds) {
 gl.ondraw = function() {
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.loadIdentity();
-  gl.translate(0, 0, 0);
+  // camera
   gl.rotate(-angleX, 1, 0, 0);
   gl.rotate(-angleY, 0, 1, 0);
   gl.translate(-camera.x, -camera.y, -camera.z);
   gl.pushMatrix();
-  gl.translate(0, -movement, 0);
+  // ball
+  gl.translate(ballPos.x, ballPos.y, ballPos.z);
+  gl.rotate(rotate, rotateAxis.x, rotateAxis.y, rotateAxis.z);
   shader.uniforms({ brightness: 1 }).draw(mesh, gl.TRIANGLES);
   shader.uniforms({ brightness: 0 }).draw(mesh, gl.LINES);
   gl.popMatrix();
+  // plane
   gl.rotate(-90, 1, 0, 0);
   gl.translate(0, 0, -10);
   shader.uniforms({ brightness: 1 }).draw(plane_mesh, gl.TRIANGLES);
@@ -85,7 +71,3 @@ gl.enable(gl.POLYGON_OFFSET_FILL);
 gl.polygonOffset(1, 1);
 gl.clearColor(0.8, 0.8, 0.8, 1);
 gl.enable(gl.DEPTH_TEST);
-
-
-
-  
