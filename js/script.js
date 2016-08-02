7537ea8d03613c32416a427fa86b08d71c818c97
Xuanyu Zhou <xuanyu@rescomp-15-281750.stanford.edu>
2016-08-01 18:08:27 -0700

    circle

--- js/script.js
+++ js/script.js
@@ -32,7 +32,73 @@ var shader2 = new GL.Shader('\
     gl_FragColor = vec4(brightness * (normal * 0.5 + 0.5), 1.0);\
   }\
 ');
+/*
+var shader_phong = new GL.Shader('\
+  varying vec4 forFragColor;\
+  const vec3 lightPos = vec3(0.0, 5.0, 0.0);\
+  const vec3 diffuseColor = vec3(0.5, 0.0, 0.0);\
+  const vec3 specColor = vec3(1.0, 1.0, 1.0);\
+  void main(){\
+    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;\
+    vec3 normal = gl_Normal;\
+    vec4 vertPos4 = modelview * vec4(inputPosition, 1.0);\
+    vec3 vertPos = vec3(vertPos4) / vertPos4.w;\
+    vec3 lightDir = normalize(lightPos - vertPos);\
+    vec3 reflectDir = reflect(-lightDir, normal);\
+    vec3 viewDir = normalize(-vertPos);\
+    float lambertian = max(dot(lightDir,normal), 0.0);\
+    float specular = 0.0;\
+    if(lambertian > 0.0) {\
+      float specAngle = max(dot(reflectDir, viewDir), 0.0);\
+      specular = pow(specAngle, 4.0);\
+    }\
+    forFragColor = vec4(lambertian*diffuseColor + specular*specColor, 1.0);\
+}\
+', '\
+  precision mediump float; \
+  varying vec4 forFragColor;\
+  void main() {\
+  gl_FragColor = forFragColor;\
+  }\
+');
 
+var shader_phong2 = new GL.Shader('\
+  #version 330 core\
+  layout (location = 0) in vec3 position;\
+  layout (location = 1) in vec3 normal;\
+  out vec3 Normal;\
+  out vec3 FragPos;\
+  uniform mat4 model;\
+  uniform mat4 view;\
+  uniform mat4 projection;\
+  void main()\
+  {\
+    gl_Position = projection * view * model * vec4(position, 1.0);\
+    FragPos = position;\
+    Normal = normal;\
+  }\
+','\
+  #version 330 core\
+  in vec3 FragPos;\
+  in vec3 Normal;    
+  uniform vec3 lightPos; 
+  uniform vec3 viewPos;
+  uniform vec3 lightColor;
+  uniform vec3 objectColor;
+  void main()
+  {
+    vec3 lightVec = normalize(lightPos - FragPos);
+    vec3 viewVec = normalize(viewPos - FragPos);
+    vec3 reflectVec = reflect(-lightVec, normalize(Normal));
+    float spec = pow(max(dot(viewVec, reflectVec), 0), 32);
+    vec3 specVec = 0.5 * spec * lightColor;
+    float diff = max(dot(normalize(Normal), lightVec), 0);
+    vec3 diffVec = diff * lightColor;
+    vec3 ambVec = 0.1 * lightColor;
+    color = vec4(((diffVec + specVec + ambVec) * objectColor), 1);
+  } 
+');
+*/
 gl.onupdate = function(seconds) {
   movement = speed * seconds + 4.9  * seconds * seconds + movement;
   speed = seconds * 9.8 + speed;
@@ -40,30 +106,17 @@ gl.onupdate = function(seconds) {
 
 function fxyz(t){
   var ret = [];
-  ret[0] = t;
-  ret[1] = (t - 5) * (t - 5);
+  ret[0] = 5 * Math.cos(t);
+  ret[1] = -5 * Math.sin(t);
   ret[2] = 0;
   return ret;
 }
 function fnormal(t){
   var ret = [];
-  var k = -1 / 2 * (t - 5);
-  if (k > 0){
-    ret[0] = 1;
-    ret[1] = -1 / 2 * (t - 5);
-    ret[2] = 0;
-  }
-  else if (k == 0){
-    ret[0] = 0;
-    ret[1] = 1;
-    ret[2] = 0
-  }
-  else{
-    ret[0] = -1;
-    ret[1] = k;
-    ret[2] = 0;
-  }
-  return ret; 
+  ret[0] = -5 * Math.cos(t);
+  ret[1] = 5 * Math.sin(t);
+  ret[2] = 0;
+  return ret;
 }
 var railMesh;
 function loadRailMesh(){
@@ -74,15 +127,17 @@ function loadRailMesh(){
   var triangles = [];
   var trianglesCount = 0;
 
-  while (t < 10){
+  while (t < 3.2){
     var current = fxyz(t);
     var c_normal = fnormal(t);
     var unit = (sphere_radius / 2) / Math.sqrt(c_normal[0] * c_normal[0] +
                                                c_normal[1] * c_normal[1] + 
                                                c_normal[2] * c_normal[2]);
+    
     var x = current[0] - unit * c_normal[0];
     var y = current[1] - unit * c_normal[1];
     var z = current[2] - unit * c_normal[2]; 
+
     vertices[verticesCount] = [x, y, z - 1];
     vertices[verticesCount + 1] = [x, y, z + 1];
     verticesCount = verticesCount + 2;
@@ -90,11 +145,11 @@ function loadRailMesh(){
   }
   for(var i = 0; i < vertices.length - 2; i++){
     if (i % 2 == 0){
-      triangles[trianglesCount] = [i, i+1, i+2];
+      triangles[trianglesCount] = [i, i+2, i+1];
       trianglesCount++;
     }
     else{
-      triangles[trianglesCount] = [i, i+2, i+1];
+      triangles[trianglesCount] = [i, i+1, i+2];
       trianglesCount++;
     }
   }
@@ -106,7 +161,6 @@ function loadRailMesh(){
   };
   var nmesh = GL.Mesh.load(data);
   railMesh = nmesh;
-  console.log(1);
   return nmesh;
 
 }
@@ -114,12 +168,12 @@ loadRailMesh();
 function drawRail(){
   gl.pushMatrix();
   gl.loadIdentity();
-  gl.translate(0, 5, -20);
-  gl.translate(-5, -12, 0);
+  gl.translate(0, 0, -20);
+  //gl.translate(-5, -12, 0);
   shader2.uniforms({brightness: 1}).draw(railMesh, gl.TRIANGLES);
   gl.popMatrix();
 }
-drawRail();
+//drawRail();
 
 gl.ondraw = function() {
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
